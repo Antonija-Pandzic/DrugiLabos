cc90cdf20ec5cde5d087c21d8b37251f1aa32a07
AntonijaPandzic123 <antonija.pandzic@fer.unizg.hr>
2025-11-07 00:24:36 +0100

    Fix build in deploying.

--- vite-project-client/src/App.tsx
+++ vite-project-client/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import XSS from './pages/XSS';
 import PasswordStorage from './pages/PasswordStorage';
 
--- vite-project-client/src/pages/XSS.tsx
+++ vite-project-client/src/pages/XSS.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 
 export default function XSS({ enabled }:{ enabled:boolean }){
